db2e500378cccbbe1b11dfdd27e4f00304e80fba
niennt04 <niennt@techzone.io.vn>
2025-04-01 07:42:54 +0700

    Add timestamps to order_items migration for tracking creation and update times

--- database/migrations/2025_03_27_031937_create_order_items_table.php
+++ database/migrations/2025_03_27_031937_create_order_items_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
             $table->foreignId('order_id')->constrained('orders');
             $table->foreignId('product_id')->constrained('products');
             $table->integer('quantity');
+            $table->timestamps();
         });
     }
 
